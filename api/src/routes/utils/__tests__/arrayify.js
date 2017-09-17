698c75b359453d297c62c814985ee1037dbf452c
Ed Gibbs <edward_gibbs@yahoo.com>
2017-09-17 15:35:58 -0700

    Another TDD exercise

--- api/src/routes/utils/__tests__/arrayify.js
+++ api/src/routes/utils/__tests__/arrayify.js
@@ -17,19 +17,24 @@
 // **Remember** to write a simple test to cover a simple use case. Then
 // write code to make that test pass. Then refactor your code to clean
 // it up if needed. Then continue the cycle until you cover all use cases.
-import arrayify from '../arrayify'
+import arrayify from '../arrayify';
 
+describe('arrayify', () => {
+  describe('when passing nothing', () => {
+    it('returns an empty array', () => {
+      expect(arrayify()).toEqual([]);
+    });
+  });
 
-//////// Elaboration & Feedback /////////
-// When you've finished with the exercises:
-// 1. Copy the URL below into your browser and fill out the form
-// 2. remove the `.skip` from the test below
-// 3. Change submitted from `false` to `true`
-// 4. And you're all done!
-/*
-http://ws.kcd.im/?ws=Testing&e=TDD&em=edward_gibbs@yahoo.com*/
-test.skip('I submitted my elaboration and feedback', () => {
-  const submitted = false // change this when you've submitted!
-  expect(true).toBe(submitted)
-})
-////////////////////////////////
+  describe('when passing a single argument', () => {
+    it('returns an array with the argument', () => {
+      expect(arrayify('hi')).toEqual(['hi']);
+    });
+  });
+
+  describe('when passing an array', () => {
+    it('returns an array with the argument', () => {
+      expect(arrayify([1, 2, 3])).toEqual([1, 2, 3]);
+    });
+  });
+});
--- api/src/routes/utils/arrayify.js
+++ api/src/routes/utils/arrayify.js
@@ -1,7 +1,11 @@
-export default arrayify
+export default arrayify;
 // Here's where you'll write arrayify. Look at
 // `api/src/routes/utils/arrayify.js` for instructions.
 
-function arrayify() {
-  // TODO don't type in here until you have a unit test first!
+function arrayify(object = []) {
+  if (Array.isArray(object)) {
+    return object;
+  } else {
+    return [object];
+  }
 }
