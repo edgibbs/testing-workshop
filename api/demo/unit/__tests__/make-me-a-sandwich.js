163e9475c728c8e84f7a50769709e551e0314fbf
Ed Gibbs <edward_gibbs@yahoo.com>
2017-09-17 14:23:35 -0700

    Write some simple exercise 2 tests

--- api/demo/unit/__tests__/make-me-a-sandwich.js
+++ api/demo/unit/__tests__/make-me-a-sandwich.js
@@ -1,1 +1,17 @@
-test('todo', () => {})
+import makeMeASandwich from '../make-me-a-sandwich';
+
+describe('#makeMeASandwich', () => {
+  describe('when sandwich is not a string', () => {
+    it('returns the sandwich', () => {
+      expect(makeMeASandwich({query: {}})).toBeNull();
+    });
+  });
+
+  describe('when sandwich is a string', () => {
+    it('returns the sandwich', () => {
+      expect(makeMeASandwich({query: {sandwich: 'Monte Cristo'}})).toEqual(
+        'Monte Cristo'
+      );
+    });
+  });
+});
